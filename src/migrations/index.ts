402d2d21b38fe0ebc666ba5a2a452fddfc0b7103
Admir <dev@admir.fi>
2026-03-18 19:49:16 +0200

    Add initial database migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260318_174839 from './20260318_174839';
+
+export const migrations = [
+  {
+    up: migration_20260318_174839.up,
+    down: migration_20260318_174839.down,
+    name: '20260318_174839'
+  },
+];
