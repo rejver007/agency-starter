70c74b78a9a3ae0701451fb4bee2bcd4c463fdc6
Admir <dev@admir.fi>
2026-03-24 00:59:49 +0200

    feat: add initial database migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260323_225914 from './20260323_225914';
+
+export const migrations = [
+  {
+    up: migration_20260323_225914.up,
+    down: migration_20260323_225914.down,
+    name: '20260323_225914'
+  },
+];
